f93b851669fc67d45562f5287091ddb8d969dacb
Ranjeet <ranjeet127ad@gmail.com>
2025-11-25 12:23:21 +0530

    Solved No. of provinces using dfs

---
+++ Graph/Questions/No_of_provinces.java
@@ -0,0 +1,45 @@
+package Graph.Questions;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class No_of_provinces {
+
+    public static int findCircleNum(int[][] isConnected) {
+        int m = isConnected.length;
+        int count = 0;
+        boolean[] vis = new boolean[m];
+
+        for(int i=0; i<m; i++){
+            if(!vis[i]){
+                bfs(i,vis,isConnected);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static void bfs(int i, boolean[] vis, int[][] isConnected) {
+        int n = isConnected.length;
+        vis[i] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(i);
+
+        while(!queue.isEmpty()){
+            int front = queue.remove();
+            for(int j=0; j<n; j++){
+                if(isConnected[front][j] == 1 && vis[j] == false){
+                    queue.add(j);
+                    vis[j] = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] isConnected = {{1,1,0},{1,1,0},{0,0,1}};
+        int ans = findCircleNum(isConnected);
+        System.out.println(ans);
+    }
+}
